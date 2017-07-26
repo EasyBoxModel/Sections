313026854ff38d2fbd44b34955daf89574294115
Gus <yo@soygus.com>
2017-07-26 13:22:07 -0500

    Updates radio fiel

--- resources/fields/radio.blade.php
+++ resources/fields/radio.blade.php
@@ -3,19 +3,19 @@
   $name = $field->getAlias();
 ?>
 
-<fieldset class="form-group {{ $errors->has($name) ? 'has-danger' : '' }}">
+<fieldset class="form-group {{ $errors->has($name) ? ' has-danger' : '' }}">
   <label for="{{ $name }}">{{ $field->getLabel() }}</label>
 
-  <div class="grid-list grid-list-2 grid-list-1-xs grid-list-1-sm">
+  <div class="grid-list {{ isset($gridListClass) ? $gridListClass : 'grid-list-2 grid-list-1-xs grid-list-1-sm' }}">
     @foreach ($field->getOptions() as $option)
       <article class="grid-list-item custom-radio-checkbox">
         <input
           id="{{ $id }}[{{ $option['key'] }}]"
-          name="{{ $name }}[]"
+          name="{{ $name }}"
           type="{{ $field->getType() }}"
           class="{{ $field->getClass() }}"
-          value="{{ old($name) ? old($name) : $option['key'] }}"
-          {{ ($option['key'] === $field->getValue()) ? 'checked' : '' }}
+          value="{{ $option['key'] }}"
+          {{ ($option['key'] === $field->getValue() || old($name) == $option['key']) ? 'checked' : '' }}
           {{ ($field->isRequired() && $loop->index == 0) ? 'required' : '' }}>
         <label for="{{ $id }}[{{ $option['key'] }}]">{{ $option['value'] }}</label>
       </article>
